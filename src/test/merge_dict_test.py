de22fa92913f93f48b013ddd1d5958bcd94cae61
errol <aojiaodage@gmail.com>
2025-04-03 15:18:33 +0800

    更新工作流配置

--- src/test/merge_dict_test.py
+++ src/test/merge_dict_test.py
@@ -2,7 +2,8 @@ if __name__ == '__main__':
     d1 = {'name': 'errol', 'p': '1'}
     d2 = {'name': 'errrrro000000l'}
     d3 = {'a': ''}
-    print(d1 | d2 | d3)
-    print(d3.get('a') is None)
-    d4 = {'name': 'ab'}
-    print("abc {}".format(d1 | d4))
+    # print(d1 | d2 | d3)
+    print(d1)
+    # print(d3.get('a') is None)
+    # d4 = {'name': 'ab'}
+    # print("abc {}".format(d1 | d4))
